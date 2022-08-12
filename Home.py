e098b18b552d39d3a971ec2d5c984bc1b1c3c353
Francisco Bustamante <23560423+chicolucio@users.noreply.github.com>
2022-08-11 22:48:02 -0300

    Add social links to Home

--- Home.py
+++ Home.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-from src.helpers import text_from_markdown, load_css
+from src.helpers import text_from_markdown, load_css, find_me_buttons
 
 st.set_page_config(layout="centered", page_title="π approximations",
                    page_icon=":chart_with_upwards_trend:")
@@ -17,3 +17,19 @@ st.image('https://upload.wikimedia.org/wikipedia/commons/4/4a/Pi-unrolled_slow.g
          caption='Animation illustrating the ratio. Source: Wikipedia')
 
 st.markdown(''.join(content[2]))
+
+sites = ("linkedin", "portfolio", "github", "github_sponsors")
+links = (
+    "flsbustamante",
+    "https://franciscobustamante.com.br",
+    "chicolucio",
+    "chicolucio",
+)
+
+columns = st.columns([1, 1, 1.2, 1, 1])
+
+with columns[2]:
+    st.write('Developed by: Francisco Bustamante')
+    st.info("Follow me for more interactive projects:")
+    for site, link in zip(sites, links):
+        find_me_buttons(site, link)
